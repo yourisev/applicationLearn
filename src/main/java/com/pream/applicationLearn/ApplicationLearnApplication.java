ac18e99f07d4c95f7a45ae9e5df79385f6fb8379
yourisev <sevgama44@gmail.com>
2023-05-10 20:15:20 -0400

    Sharing project on Github!

---
+++ src/main/java/com/pream/applicationLearn/ApplicationLearnApplication.java
@@ -0,0 +1,13 @@
+package com.pream.applicationLearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationLearnApplication.class, args);
+	}
+
+}
